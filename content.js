ceb9a0a27f4de406b61bf8a6f741e2b846bf8d59
David Pichsenmeister <git@pichsenmeister.com>
2021-06-09 17:56:21 -0700

    added audio support

--- content.js
+++ content.js
@@ -7,6 +7,7 @@ const clickHandler = (event) => {
         origin: { x: x, y: y },
         zIndex: 99999
     })
+    playSound()
 }
 
 const buttons = document.querySelectorAll('button,input[type="submit"],a')
@@ -18,3 +19,13 @@ buttons.forEach(button => {
 // for (i = 0; i < buttons.length; i++) {
 //     buttons[i].addEventListener('click', clickHandler)
 // }
+
+const audioUrl = 'https://raw.githubusercontent.com/dpiTech/formfun/main/party-horn.wav'
+const audio = new Audio(audioUrl)
+const playSound = () => {
+    if (!audio.paused) {
+        audio.pause()
+        audio.currentTime = 0
+    }
+    audio.play()
+}
